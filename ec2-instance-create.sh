f80874ed5698ea9e317532ff418b42852484c68b
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-09-11 00:08:58 +0530

    ec2 instance msg

--- ec2-instance-create.sh
+++ ec2-instance-create.sh
@@ -3,5 +3,12 @@
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
 for name in ${instances[@]}; do
-echo "instance name is: $name"
+if [ $name == "shipping" ] || [ $name == "mysql" ]
+then 
+  instance_type="t3.medium"
+else
+  instance_type="t3.micro"
+fi
+
+echo "Creating instance for: $name with instance type: $instance_type"
 done

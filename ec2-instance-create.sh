22f291033e9eabf0d4db7728fd57096d9c509a57
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-09-11 00:04:12 +0530

    ec2 instance msg

--- ec2-instance-create.sh
+++ ec2-instance-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-instances=("mongodb" "redis" "mysql")
+instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
 for name in ${instances[@]}; do
-echo "instance is: $name"
+echo "instance name is: $name"
 done
